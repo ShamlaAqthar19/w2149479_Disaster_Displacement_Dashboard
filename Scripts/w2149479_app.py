225e515adbc6f280a1460f68aaacc3fbe0670284
ShamlaAqthar19 <shamlaaqthar19@gmail.com>
2026-04-21 22:37:07 +0530

    Added filters and changes the visual colours in the dashboard

--- Scripts/w2149479_app.py
+++ Scripts/w2149479_app.py
@@ -11,9 +11,23 @@ df = load_data()
 #dashboard heading and description
 st.title('Disaster Displacement Analysis')
 st.markdown(f'This dashboard analyses **{len(df):,}** recorded disaster displacement events globally')
+#adding a sidebar for filters 
+st.sidebar.header('Filters')
+#year selector
+years = st.sidebar.slider('Years', int(df['year'].min()),
+                          int(df['year'].max()),
+                          (int(df['year'].min()), int(df['year'].max())))
+#hazard selector
+hazards = df['hazard_type_name'].unique()
+hazard_list = st.sidebar.multiselect('Hazard Type', options=hazards, default=hazards)
+#applying the filter
+filter_df = df[(df['year']>= years[0]) &
+               (df['year']<= years[1])&
+               (df['hazard_type_name'].isin(hazard_list))]
+
 #KPI chart to show the total number of displacments across the years and the most contributing hazard type
-sum_displacement = df['new_displacement'].sum()
-hazard_top = df.groupby('hazard_type_name')['new_displacement'].sum().idxmax()
+sum_displacement = filter_df['new_displacement'].sum()
+hazard_top = filter_df.groupby('hazard_type_name')['new_displacement'].sum().idxmax()
 col_1,col_2 = st.columns(2)
 with col_1:
     st.metric('Total people displaced', f'{sum_displacement:,.0f}')
@@ -22,25 +36,25 @@ with col_2:
 #trend line to see how many accidents happened across the years
 st.subheader('Displacement Trend over the years')
 #grouping the displacements by year (2008-2024)
-trend = df.groupby('year')['new_displacement'].sum().reset_index()
-plotfig = px.line(trend, x='year',y='new_displacement',markers=True)
+trend = filter_df.groupby('year')['new_displacement'].sum().reset_index()
+plotfig = px.line(trend, x='year',y='new_displacement',markers=True, color_discrete_sequence=['darkorange'])
 st.plotly_chart(plotfig,use_container_width=True)
 #bar chart to show the no of displacments by hazard types
 st.subheader('Displacements by Hazard')
-no_of_hazard = df.groupby('hazard_type_name')['new_displacement'].sum().reset_index().sort_values('new_displacement', ascending=False)
+no_of_hazard = filter_df.groupby('hazard_type_name')['new_displacement'].sum().reset_index().sort_values('new_displacement', ascending=False)
 #kept in ascending order for easiness
 barfig = px.bar(no_of_hazard, x='new_displacement', y='hazard_type_name', orientation='h',color = 'hazard_type_name')
 st.plotly_chart(barfig, use_container_width=True)
 #Top 10 affected countries
 st.subheader('Top 10 Most Affected Countries')
-top_10 = df.groupby('country_name')['new_displacement'].sum().reset_index().nlargest(10,'new_displacement')
-topfig = px.bar(top_10, x='country_name', y='new_displacement', color ='new_displacement', color_continuous_scale='Reds')
+top_10 = filter_df.groupby('country_name')['new_displacement'].sum().reset_index().nlargest(10,'new_displacement')
+topfig = px.bar(top_10, x='country_name', y='new_displacement', color ='new_displacement', color_continuous_scale='Oranges')
 st.plotly_chart(topfig,use_container_width=True)
 #ploting a map 
 st.subheader('Global Distribution of Displacements')
 #grouping displacements by country
-map = df.groupby('country_name')['new_displacement'].sum().reset_index()
-mapfig = px.choropleth(map,locations='country_name', locationmode='country names', color='new_displacement',hover_name='country_name',color_continuous_scale=px.colors.sequential.Plasma)
+map = filter_df.groupby('country_name')['new_displacement'].sum().reset_index()
+mapfig = px.choropleth(map,locations='country_name', locationmode='country names', color='new_displacement',hover_name='country_name',color_continuous_scale=px.colors.sequential.Oranges)
 st.plotly_chart(mapfig, use_container_width=True)
 
 
